21d7706c0645eb22445e3fa36ad73934731cc7b9
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-08-30 22:10:25 -0300

    tamano de pantalla dinamico

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 // Services
 import { SidenavService } from '../services/shared/sidenav.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pages',
@@ -10,35 +11,34 @@ import { SidenavService } from '../services/shared/sidenav.service';
 })
 export class PagesComponent{
 
-  windows:number[];
+  windows:number[] = [1000, 1000];
 
   constructor( public _sidenav: SidenavService ){
-    this.windows = this.tamVentana();
+    this.tamWindows().subscribe( (resp:any[]) => {
+      this.windows = resp;
+    });
   }
 
-  
 
-  tamVentana() {
-    var tam = [0, 0];
-    if (typeof window.innerWidth != 'undefined')
-    {
-      tam = [window.innerWidth,window.innerHeight];
-    }
-    else if (typeof document.documentElement != 'undefined'
-        && typeof document.documentElement.clientWidth !=
-        'undefined' && document.documentElement.clientWidth != 0)
-    {
-      tam = [
-          document.documentElement.clientWidth,
-          document.documentElement.clientHeight
-      ];
-    }
-    else   {
-      tam = [
-          document.getElementsByTagName('body')[0].clientWidth,
-          document.getElementsByTagName('body')[0].clientHeight
-      ];
-    }
-    return tam;
+  // Adjusts the size of the screen to the device on which it is used every 0.5s
+  tamWindows() {
+    return new Observable( observer => {
+      setInterval( () => {
+        var tam = [0, 0];
+        if (typeof window.innerWidth != 'undefined'){
+          tam = [window.innerWidth,window.innerHeight];
+        }else if (typeof document.documentElement != 'undefined'
+            && typeof document.documentElement.clientWidth !=
+            'undefined' && document.documentElement.clientWidth != 0){
+          tam = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+        }else   {
+          tam = [
+              document.getElementsByTagName('body')[0].clientWidth,
+              document.getElementsByTagName('body')[0].clientHeight
+          ];
+        }
+        observer.next(tam);
+      }, 500)
+    });
   }
 }
