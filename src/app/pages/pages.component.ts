53316bbac384b0a825da8d9e445c1d6fe3468e0d
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-09-26 20:56:40 -0300

    Tamano de pantalla optimizado quitando observable y aplicando CSS3

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 // Services
 import { SidenavService } from '../services/shared/sidenav.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pages',
@@ -11,34 +10,5 @@ import { Observable } from 'rxjs';
 })
 export class PagesComponent{
 
-  windows:number[] = [1000, 1000];
-
-  constructor( public _sidenav: SidenavService ){
-    this.tamWindows().subscribe( (resp:any[]) => {
-      this.windows = resp;
-    });
-  }
-
-
-  // Adjusts the size of the screen to the device on which it is used every 0.5s
-  tamWindows() {
-    return new Observable( observer => {
-      setInterval( () => {
-        var tam = [0, 0];
-        if (typeof window.innerWidth != 'undefined'){
-          tam = [window.innerWidth,window.innerHeight];
-        }else if (typeof document.documentElement != 'undefined'
-            && typeof document.documentElement.clientWidth !=
-            'undefined' && document.documentElement.clientWidth != 0){
-          tam = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-        }else   {
-          tam = [
-              document.getElementsByTagName('body')[0].clientWidth,
-              document.getElementsByTagName('body')[0].clientHeight
-          ];
-        }
-        observer.next(tam);
-      }, 500)
-    });
-  }
+  constructor( public _sidenav: SidenavService ){}
 }
